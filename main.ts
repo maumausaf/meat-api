bc4789f758d44041c7759477504c013a32f58921
Martins Maurílio <mauriliosfc@gmail.com>
2018-08-26 20:32:13 -0300

    aula-18

---
+++ main.ts
@@ -0,0 +1,17 @@
+import {Server} from './server/server'
+import {error} from "util";
+
+const server = new Server()
+
+server.bootstrap().then(server=>{
+    console.log('Server is listening on:', server.application.address())
+}).catch(error=>{
+    console.log('Server failed to start')
+    console.error(error)
+    process.exit(1)
+})
+
+
+
+
+
